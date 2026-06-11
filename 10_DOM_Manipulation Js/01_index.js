69059e0af3c68980737e385fb8618b2069821cc7
vraj206919 <vrajd9313@gmail.com>
2026-06-11 12:05:23 +0530

    Add files via upload

---
+++ 10_DOM_Manipulation Js/01_index.js
@@ -0,0 +1,7 @@
+// id selector
+
+document.getElementById("heading").innerText = "This is heading";
+
+// class selector
+
+let Workflow = document.getElementsByClassName("tag1")[0].innerHTML ="H1 is Heading 1";
