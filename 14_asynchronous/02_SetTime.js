539ac3963f7635e7f196c5bf8e22c30d81395d5c
vraj206919 <vrajd9313@gmail.com>
2026-06-18 11:04:06 +0530

    Add files via upload

---
+++ 14_asynchronous/02_SetTime.js
@@ -0,0 +1,18 @@
+
+// Set time Mean Time setter in output .
+
+setTimeout(function(){
+
+    console.log("Ummmmmmmm");
+},3000);
+
+
+setTimeout(function(){
+
+    console.log("Nikli Gyaaaa");
+},4000)
+
+setTimeout(function(){
+
+    console.log("Dakuuuuuu");
+},6000)
